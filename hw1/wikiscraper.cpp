a469befeaa74407adcd9d3c57ee96dbae3e67d5e
ruiyan-ma <maruiyan17@gmail.com>
2023-11-14 23:39:10 +0800

    hw1 part a

--- hw1/wikiscraper.cpp
+++ hw1/wikiscraper.cpp
@@ -17,7 +17,7 @@ using std::unordered_map;   using std::unordered_set;
  * If you used any helper functions, just put them above this function.
  */
 
-// TODO: ASSIGNMENT 2 TASK 4:
+// ASSIGNMENT 2 TASK 4:
 // Please implement a function that can determine if a wiki link is valid or not.
 // As a reminder, it needs to take in a string and return whether or not 
 // # or : is contained in the string.
@@ -26,9 +26,7 @@ using std::unordered_map;   using std::unordered_set;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // BEGIN STUDENT CODE HERE
 bool valid_wikilink(const string& link) {
-    // replace these lines!
-    (void) link;
-    throw std::invalid_argument("Not implemented yet.\n");
+    return std::all_of(link.begin(), link.end(), [](char c){return c != ':' && c != '#';});
 }
 // END STUDENT CODE HERE
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,15 +34,13 @@ bool valid_wikilink(const string& link) {
 unordered_set<string> findWikiLinks(const string& inp) {
     /* Delimiter for start of a link  */
     static const string delim = "href=\"/wiki/";
-
     unordered_set<string> ret;
-
     auto url_start = inp.begin();
     auto end = inp.end();
 
     while(true) {
 
-        // TODO: ASSIGNMENT 2 TASK 1:
+        // ASSIGNMENT 2 TASK 1:
         // Set url_start to the next location of "delim" (starting your search at url_start), using std::search.
         // After doing so, break out of the while loop if there are no occurrences of delim left
         // (use your work from the line above).
@@ -52,32 +48,32 @@ unordered_set<string> findWikiLinks(const string& inp) {
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         // BEGIN STUDENT CODE HERE
-        // Please delete this line when you start working!
-        throw std::invalid_argument("Not implemented yet.\n");
+        auto url_start = std::search(url_start, end, delim.begin(), delim.end());
+        if (url_start == end) break;
         // END STUDENT CODE HERE
         ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // TODO: ASSIGNMENT 2 TASK 2:
+        // ASSIGNMENT 2 TASK 2:
         // Set url_end to the end of the wikilink. Start searching after the delimeter you found above.
         // Make sure to use std::find! (std::find looks for a single element in a container, e.g. character in 
-        // a string—std::search looks for a series of elements in a container, like a substring in a string. 
+        // a string, std::search looks for a series of elements in a container, like a substring in a string. 
         // remember that a string is represented as an array of characters, and is also a container!)
         // Estimated length: 1 lines
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         // BEGIN STUDENT CODE HERE (delete/edit this line)
-        auto url_end = url_start;
+        auto url_end = std::find(url_start + delim.length(), end, '"');
         // END STUDENT CODE HERE
         ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // TODO: ASSIGNMENT 2 TASK 3:
+        // ASSIGNMENT 2 TASK 3:
         // Last exercise of this function! Create a string from the two iterators (url_start and url_end) above
         // using a string constructor. Make sure you start the string AFTER the delimiter you found in task 5!
         // Estimated length: 1 lines
         
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         // BEGIN STUDENT CODE HERE (delete/edit this line)
-        string link;
+        string link = string(url_start + delim.length(), url_end);
         // END STUDENT CODE HERE
         ///////////////////////////////////////////////////////////////////////////////////////////////////
 
